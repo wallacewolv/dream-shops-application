5ac16fd52ce94e5c078becf07bf5ec2c2a7ec16b
Wallace W <wallace_wolv@hotmail.com>
2025-09-10 22:27:33 -0300

    feat: adiciona entidades Category, Product e Image com relacionamentos JPA
    - Cria entidade Category com relação @OneToMany para Product
    - Cria entidade Product com chave primária, atributos e relações:
      - @ManyToOne para Category
      - @OneToMany para Image
    - Cria entidade Image com atributos, @Lob para blob e relação @ManyToOne para Product
    - Usa Lombok para geração de getters, setters e construtores

--- src/main/java/com/wwdev/dream_shops/model/Image.java
+++ src/main/java/com/wwdev/dream_shops/model/Image.java
@@ -1,13 +1,30 @@
 package com.wwdev.dream_shops.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.sql.Blob;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileName;
     private String fileType;
+
+    @Lob
     private Blob blob;
     private String downloadUrl;
 
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
 }
--- src/main/java/com/wwdev/dream_shops/model/Product.java
+++ src/main/java/com/wwdev/dream_shops/model/Product.java
@@ -1,9 +1,22 @@
 package com.wwdev.dream_shops.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String brand;
@@ -11,7 +24,10 @@ public class Product {
     private int inventory;
     private String description;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
     private Category category;
 
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Image> images;
 }
