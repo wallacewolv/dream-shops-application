1197d0af2a4c907b7bd9e641c5b00db2f72790cc
Wallace W <wallace_wolv@hotmail.com>
2025-09-11 01:30:06 -0300

    feat: Interface para implementação do ProductService

---
+++ src/main/java/com/wwdev/dream_shops/service/product/IProductService.java
@@ -0,0 +1,19 @@
+package com.wwdev.dream_shops.service.product;
+
+import com.wwdev.dream_shops.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct(Product product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product, Long productId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+}
